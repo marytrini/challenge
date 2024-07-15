c7a8dbc22f31b4fbc25d6293cd1d2d2779ce31dc
Maria Salazar <mariatrinidadsalazarr@gmail.com>
2024-07-15 02:02:04 -0400

    cors_permissions_updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,37 @@
 const express = require("express");
-const morgan = require('morgan');
+const morgan = require("morgan");
 require("dotenv").config();
-const router = require('./routes/index');
+const router = require("./routes/index");
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-        "Access-Control-Allow-Headers",
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://notes-ghruyxvqw-marytrinis-projects.vercel.app/"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 //app.set("port", process.env.PORT);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/",router);
+app.use("/", router);
 
-app.use((err, req, res, next)=>{
-    const status = err.status || 500;
-    const message = err.message || err;
-    console.error(err);
-    res.status(status).send(message);
-})
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = app;
